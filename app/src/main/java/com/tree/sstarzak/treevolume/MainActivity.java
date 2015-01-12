3da60b6aaa8f186b1951b1d52a83232cf3e8a5f7
Szymon Starzak <szymonstarzak@gmail.com>
2015-01-12 02:49:13 +0100

    Wyswietlanie wszystkich wynikow oprocz estymowanych

--- app/src/main/java/com/tree/sstarzak/treevolume/MainActivity.java
+++ app/src/main/java/com/tree/sstarzak/treevolume/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
-    Button button1, button2, button3, button4, button5,button6, button7, button8,button_info;
+    Button button1, button2, button3, button4, button5, button6, button7, button8, button_info;
 
     @Override
     protected void onResume() {
@@ -19,6 +19,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         Measurement m = Measurement.listAll(Measurement.class).get(0);
 
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,53 +53,68 @@ public class MainActivity extends Activity implements View.OnClickListener {
         button8.setOnClickListener(this);
 
     }
+
     @Override
     public void onClick(View view) {
         Button b = (Button) view;
         Intent intent = null;
         GradientDrawable gd = new GradientDrawable();
         gd.setColor(Color.parseColor("#32cd32"));
-        gd.setStroke(1,Color.BLACK);
+        gd.setStroke(1, Color.BLACK);
 
         switch (b.getId()) {
-            case R.id.button_info: intent = new Intent(this, HowToMeasureActivity.class); break;
-            case R.id.button:  intent = new Intent(this, GetHeightActivity.class); break;
+            case R.id.button_info:
+                intent = new Intent(this, HowToMeasureActivity.class);
+                break;
+            case R.id.button:
+                intent = new Intent(this, GetHeightActivity.class);
+                break;
             case R.id.button2:
                 intent = new Intent(this, GetDistanceActivity.class);
-                intent.putExtra("m_type",0);
+                intent.putExtra("m_type", 0);
                 break;
-            case R.id.button3: intent = new Intent(this, GetTreeLengthActivity.class);break;
+            case R.id.button3:
+                intent = new Intent(this, GetTreeLengthActivity.class);
+                break;
             case R.id.button4:
                 intent = new Intent(this, GetDistanceActivity.class);
-                intent.putExtra("m_type",1);
+                intent.putExtra("m_type", 1);
                 break;
             case R.id.button5:
                 intent = new Intent(this, GetDendrochronology.class);
-                intent.putExtra("d_type",0);
+                intent.putExtra("d_type", 0);
                 break;
             case R.id.button6:
                 intent = new Intent(this, GetDistanceActivity.class);
-                intent.putExtra("m_type",2);
+                intent.putExtra("m_type", 2);
                 break;
             case R.id.button7:
                 intent = new Intent(this, GetDendrochronology.class);
-                intent.putExtra("d_type",1);
+                intent.putExtra("d_type", 1);
                 break;
             case R.id.button8:
                 Measurement m = Measurement.listAll(Measurement.class).get(0);
 
-                 TextView tv = (TextView) findViewById(R.id.textView);
-                 tv.setText("Max Object Volume: " + String .valueOf(
-                         ((m.getMax_volume_back_side()/100 + m.getMax_volume_front_side()/100)/2) * (m.getObject_length()/100)
-                 ) + " m^3\n");
-                tv.append("Estimated Tree Volume: "+String.valueOf(
-                                ((m.getVolume_back_side()/100 + m.getVolume_front_side()/100)/2) * (m.getObject_length()/100)
+                TextView tv = (TextView) findViewById(R.id.textView);
+                tv.setText("");
+                tv.append("Device height: " + String.valueOf(m.getDevice_height()/100) + " m\n" );
+                tv.append("Distance from side: " + String.valueOf(m.getDistance_from_side()/100)+ " m\n");
+                tv.append("Object length: " + String.valueOf(m.getObject_length()/100)+ " m\n");
+                tv.append("Distance from back: " + String.valueOf(m.getDistance_from_first_d()/100)+ " m\n");
+                tv.append("Distance from front: " + String.valueOf(m.getDistance_from_secont_d()/100)+ " m\n");
+                tv.append("Surface max area1: " + String.valueOf(m.getMax_volume_back_side()/100)+ " m\n");
+                tv.append("Surface max area2: " + String.valueOf(m.getMax_volume_front_side()/100)+ " m\n");
+                tv.append("Max Object Volume: " + String.valueOf(
+                        ((m.getMax_volume_back_side() / 100 + m.getMax_volume_front_side() / 100) / 2) * (m.getObject_length() / 100)
+                ) + " m^3\n");
+                tv.append("Estimated Tree Volume: " + String.valueOf(
+                                ((m.getVolume_back_side() / 100 + m.getVolume_front_side() / 100) / 2) * (m.getObject_length() / 100)
                         ) + " m^3\n"
                 );
                 break;
         }
-        if(intent != null)
-        startActivity(intent);
+        if (intent != null)
+            startActivity(intent);
         b.setBackground(gd);
     }
 }
