79969b9ee8ca5b4bc37252ed7c5c450e3a18db2d
Szymon Starzak <szymonstarzak@gmail.com>
2015-01-24 01:22:45 +0100

    Dodanie operacji morflogicznych

--- app/src/main/java/com/tree/sstarzak/treevolume/GetDendrochronology2.java
+++ app/src/main/java/com/tree/sstarzak/treevolume/GetDendrochronology2.java
@@ -3,19 +3,26 @@ package com.tree.sstarzak.treevolume;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -29,10 +36,12 @@ public class GetDendrochronology2 extends Activity implements SeekBar.OnSeekBarC
     SeekBar sb3;
     SeekBar sb4;
     SeekBar sb5;
+    SeekBar sb6;
     Bitmap theOriginalImage;
     Bitmap binarizedImage;
     ImageView iv;
     TextView tv;
+    Button opening,closeing,erode,dilatate,reset;
     int contrast = 0;
     int brightness = 0;
     int all_px;
@@ -64,10 +73,29 @@ public class GetDendrochronology2 extends Activity implements SeekBar.OnSeekBarC
         sb5.setOnSeekBarChangeListener(this);
         sb5.setMax(255);
 
+        sb6 = (SeekBar) findViewById(R.id.seekBar6);
+        sb6.setOnSeekBarChangeListener(this);
+        sb6.setMax(14);
+
         theOriginalImage = BitmapFactory.decodeResource(this.getResources(), ID_RES);
         binarizedImage = theOriginalImage.copy(theOriginalImage.getConfig(), true);
 
         tv = (TextView) findViewById(R.id.textView1);
+
+        opening = (Button) findViewById(R.id.open);
+        opening.setOnClickListener(this);
+
+        closeing = (Button) findViewById(R.id.close);
+        closeing.setOnClickListener(this);
+
+        erode =  (Button) findViewById(R.id.erode);
+        erode.setOnClickListener(this);
+
+        dilatate = (Button) findViewById(R.id.dilatate);
+        dilatate.setOnClickListener(this);
+
+        reset = (Button) findViewById(R.id.reset);
+        reset.setOnClickListener(this);
 
         iv = (ImageView) findViewById(R.id.imageView1);
         iv.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), ID_RES));
@@ -127,12 +155,29 @@ public class GetDendrochronology2 extends Activity implements SeekBar.OnSeekBarC
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
+
+       switch (seekBar.getId()) {
+           case R.id.seekBar1:
+           case R.id.seekBar2:
+           case R.id.seekBar3:
+           case R.id.seekBar4:
+           case R.id.seekBar5:
+
+               BinarizeImage();
+               white_px = 0;
+               all_px = binarizedImage.getWidth() * binarizedImage.getHeight();
+
+               break;
+       }
+        tv.setText(String.valueOf(white_px) + '/' + String.valueOf(all_px));
+        iv.setImageBitmap(binarizedImage);
+    }
+
+    public void BinarizeImage() {
         binarizedImage = changeBitmapContrastBrightness(
                 theOriginalImage.copy(theOriginalImage.getConfig(), true),
                 contrast,
                 brightness);
-        all_px = white_px = 0;
-        all_px = binarizedImage.getWidth() * binarizedImage.getHeight();
         for (int i = 0; i < binarizedImage.getWidth(); i++) {
             for (int c = 0; c < binarizedImage.getHeight(); c++) {
                 int pixel = binarizedImage.getPixel(i, c);
@@ -144,16 +189,45 @@ public class GetDendrochronology2 extends Activity implements SeekBar.OnSeekBarC
                 }
             }
         }
-        tv.setText(String.valueOf(white_px) + '/' + String.valueOf(all_px));
-        iv.setImageBitmap(binarizedImage);
     }
-
     @Override
     public void onClick(View view) {
-        Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        photoPickerIntent.setType("image/jpg");
-        startActivityForResult(photoPickerIntent, SELECT_PHOTO);
-    }
+        System.loadLibrary("opencv_java");
+        switch (view.getId()) {
+            case R.id.imageView1:   Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                                    photoPickerIntent.setType("image/jpg");
+                                    startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+                break;
+            default:
+                Mat src = new Mat(binarizedImage.getWidth(), binarizedImage.getHeight(), CvType.CV_8UC1);
+                Utils.bitmapToMat(binarizedImage.copy(binarizedImage.getConfig(),true),src);
+                Mat kernel = Imgproc.getStructuringElement( Imgproc.CV_SHAPE_ELLIPSE, new Size(sb6.getProgress()+1,sb6.getProgress()+1));
+                Mat dst = new Mat();
+                switch (view.getId()) {
+                    case R.id.open:
+                        Imgproc.morphologyEx(src,dst, Imgproc.MORPH_OPEN, kernel);
+                        Utils.matToBitmap(dst,binarizedImage);
+                        break;
+                    case R.id.close:
+                        Imgproc.morphologyEx(src,dst, Imgproc.MORPH_CLOSE, kernel);
+                        Utils.matToBitmap(dst,binarizedImage);
+                        break;
+                    case R.id.erode:
+                        Imgproc.erode(src,dst, kernel);
+                        Utils.matToBitmap(dst,binarizedImage);
+                        break;
+                    case R.id.dilatate:
+                        Imgproc.dilate(src,dst, kernel);
+                        Utils.matToBitmap(dst,binarizedImage);
+                        break;
+                    case R.id.reset:
+                        BinarizeImage();
+                        break;
+                }
+                iv.setImageBitmap(binarizedImage);
+                break;
+        }
+        }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
